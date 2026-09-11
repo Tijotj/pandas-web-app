164f4a8e3ce3041c964845c1f9e10090443f5295
Tijotj <58003665+Tijotj@users.noreply.github.com>
2026-09-11 13:32:17 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import streamlit as st
+
+st.title("My CSV / Excel Processor")
+uploaded_file = st.file_uploader("Choose a CSV or Excel file", type=["csv","xlsx"])
+
+if uploaded_file is not None:
+	if uploaded_file.name.endswith(".csv"):
+		df = pd.read_csv(uploaded_file) 
+	else:
+		df = pd.read_excel(uploaded_file)
+	st.write("### Original Data ")
+	st.dataframe(df)
+
+	st.write("### Processed Data ")
+	st.write(df.describe())
+ 
